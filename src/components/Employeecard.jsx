7d3d6cb96b72a2b8bf8a2a6fc2fe93903cc3d901
PraveenAkashk <praveenkadhiravan52853@gmail.com>
2024-06-23 08:23:43 +0530

    skeleton added

---
+++ src/components/Employeecard.jsx
@@ -0,0 +1,53 @@
+let Employeecard=()=>{
+
+    return(
+        <div>
+            <div className="img-wrap">
+                <img src="" alt="" />
+            </div>
+            <div className="detail-wrap">
+                <div className="name">Praveen</div>
+                <div className="info-wrap">
+                    <div className="info-card">
+                        <div className="logo-wrap">
+                            <img src="" alt="" />
+                        </div>
+                        <div className="data-wrap">
+                            <div className="title">Role</div>
+                            <div className="value">Front end developer</div>
+                        </div>
+                    </div>
+                    <div className="info-card">
+                        <div className="logo-wrap">
+                            <img src="" alt="" />
+                        </div>
+                        <div className="data-wrap">
+                            <div className="title">Phone Number</div>
+                            <div className="value">9500691394</div>
+                        </div>
+                    </div>
+                    <div className="info-card">
+                        <div className="logo-wrap">
+                            <img src="" alt="" />
+                        </div>
+                        <div className="data-wrap">
+                            <div className="title">Email Address</div>
+                            <div className="value">
+                                <a href="mailmeto:praveenkadhiravan52853@gmail.com">praveenkadhiravan52853@gmail.com</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="info-card">
+                        <div className="logo-wrap"></div>
+                        <div className="data-wrap">
+                            <div className="title"></div>
+                            <div className="value"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Employeecard;
